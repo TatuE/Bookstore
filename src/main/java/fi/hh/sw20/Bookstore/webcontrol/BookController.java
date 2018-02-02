7b5d164a03ae2efc038df285682e42d01c70e9b7
Tatu Erkinjuntti <tatu.erkinjuntti@gmail.com>
2018-02-02 15:10:07 +0200

    Added booklist.html , updated BookController

--- src/main/java/fi/hh/sw20/Bookstore/webcontrol/BookController.java
+++ src/main/java/fi/hh/sw20/Bookstore/webcontrol/BookController.java
@@ -1,17 +1,23 @@
 package fi.hh.sw20.Bookstore.webcontrol;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import fi.hh.sw20.Bookstore.domain.Book;
+import fi.hh.sw20.Bookstore.domain.BookRepository;
+
+
 
 @Controller
 public class BookController {
-	@RequestMapping(value="/index", method=RequestMethod.GET)
-	public String books(Model model, Book book) {
-		return "index";
+	@Autowired
+	private BookRepository bookRepository;
+	@RequestMapping(value="/booklist", method=RequestMethod.GET)
+	public String books(Model model) {
+		model.addAttribute("book", bookRepository.findAll());
+		return "booklist";
 	}
 
 }
