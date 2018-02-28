ec60d47b2ef0bc16fc0df8dbd268cc858742cb50
Tatu Erkinjuntti <tatu.erkinjuntti@gmail.com>
2018-02-28 15:11:09 +0200

    Added RESTful service to Controller, Rest -> /api

--- src/main/java/fi/hh/sw20/Bookstore/webcontrol/BookController.java
+++ src/main/java/fi/hh/sw20/Bookstore/webcontrol/BookController.java
@@ -1,11 +1,14 @@
 package fi.hh.sw20.Bookstore.webcontrol;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.hh.sw20.Bookstore.domain.Book;
 import fi.hh.sw20.Bookstore.domain.BookRepository;
@@ -23,7 +26,7 @@ public class BookController {
 	
 	@RequestMapping(value="/booklist", method=RequestMethod.GET)
 	public String books(Model model) {
-		model.addAttribute("book", bookRepository.findAll());
+		model.addAttribute("books", bookRepository.findAll());
 		return "booklist";
 	}
 	
@@ -49,6 +52,7 @@ public class BookController {
 	@RequestMapping(value="/editbook/{id}", method=RequestMethod.GET)
 	public String editBook(@PathVariable("id") Long bookId, Model model) {
     	model.addAttribute("book", bookRepository.findOne(bookId));
+    	model.addAttribute("category", categoryRepository.findAll());
         return "editbook";
     }
 	
@@ -56,5 +60,15 @@ public class BookController {
 	public String editBook(Book book) {
 		bookRepository.save(book);
 		return "redirect:/booklist";
-	}	
+	}
+	
+	@RequestMapping(value="/books", method=RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest(){
+		return (List<Book>) bookRepository.findAll();		
+	}
+	
+	@RequestMapping(value="/book/{id}", method=RequestMethod.GET)
+	public @ResponseBody Book findBookRest(@PathVariable("id") Long id) {
+		return bookRepository.findOne(id);
+	}
 }
